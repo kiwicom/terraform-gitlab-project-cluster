41d3eff107b83ef9b03203599dbaf50b97552a65
Adam Janis <adam.janis@kiwi.com>
2019-07-25 18:38:32 +0000

    platform: tf 0.12 k8s modules
    kiwicom-source-id: 917b48fefc2007ebecdd14208c59ca2826cd970e

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 data "external" "environment_scope" {
-  program = ["${path.module}/environment_scope.sh"]
+  program = [
+    "${path.module}/environment_scope.sh"]
 
   query = {
-    gitlab_environment_scope = "${var.gitlab_environment_scope}"
-    google_project           = "${var.google_project}"
-    cluster_name             = "${var.cluster_name}"
-    namespace                = "${var.kubernetes_ns_info["name"]}"
+    gitlab_environment_scope = var.gitlab_environment_scope
+    google_project           = var.google_project
+    cluster_name             = var.cluster_name
+    namespace                = var.kubernetes_ns_info["name"]
   }
 }
 
 resource "gitlab_project_cluster" "gitlab_cluster" {
-  project                       = "${var.gitlab_project_id}"
-  name                          = "${var.cluster_name}"
-  domain                        = "${var.cluster_base_domain}"
-  enabled                       = "${var.enabled}"
-  managed                       = "${var.cluster_managed}"
+  project                       = var.gitlab_project_id
+  name                          = var.cluster_name
+  domain                        = var.cluster_base_domain
+  enabled                       = var.enabled
+  managed                       = var.cluster_managed
   kubernetes_api_url            = "https://${var.kubernetes_api_url}"
-  kubernetes_token              = "${lookup(var.kubernetes_ns_ci_secret["data"], "token")}"
-  kubernetes_ca_cert            = "${trimspace(base64decode(var.kubernetes_ca_cert))}"
-  kubernetes_namespace          = "${var.kubernetes_ns_info["name"]}"
+  kubernetes_token              = var.kubernetes_ns_ci_secret["data"]["token"]
+  kubernetes_ca_cert            = trimspace(base64decode(var.kubernetes_ca_cert))
+  kubernetes_namespace          = var.kubernetes_ns_info["name"]
   kubernetes_authorization_type = "rbac"
-  environment_scope             = "${data.external.environment_scope.result["environment_scope"]}"
+  environment_scope             = data.external.environment_scope.result["environment_scope"]
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,31 @@
-variable "cluster_name" {}
-variable "gitlab_project_id" {}
+variable "cluster_name" {
+}
+
+variable "gitlab_project_id" {
+}
 
 variable "gitlab_environment_scope" {
   description = "One of 'gitlab_environment_scope' or 'google_project' has to be specified."
-  default = ""
+  default     = ""
 }
 
 variable "google_project" {
   description = "One of 'gitlab_environment_scope' or 'google_project' has to be specified."
-  default = ""
+  default     = ""
 }
-
 
 variable "cluster_base_domain" {
   default = "dummy.set.in.infra.repo.example.com"
 }
-variable "kubernetes_api_url" {}
-variable "kubernetes_ca_cert" {}
+
+variable "kubernetes_api_url" {
+}
+
+variable "kubernetes_ca_cert" {
+}
 
 variable "kubernetes_ns_info" {
-  type = "map"
+  type = map(string)
 }
 
 variable "kubernetes_authorization_type" {
@@ -31,7 +37,7 @@ variable "enabled" {
 }
 
 variable "kubernetes_ns_ci_secret" {
-  type = "map"
+  type = map(map(string))
 }
 
 variable "cluster_managed" {
